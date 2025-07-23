59258fbd643da9e0d232a59382600ed3183464e3
Jan Brauner <you@example.com>
2025-07-23 20:02:34 +0200

    removed sidebar and added overlay

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,12 @@
 function toggleSidebar() {
     document.getElementById("pkm-sidebar").classList.toggle("active")
 }
+
+
+function openOverlay() {
+    let refOverlay = document.getElementById("overlay");
+    // let hideScrollbar = document.getElementById("body-scrollbar");
+    // hideScrollbar.classList.add("hide-scrollbar");
+    refOverlay.classList.remove("d-none");
+
+}
