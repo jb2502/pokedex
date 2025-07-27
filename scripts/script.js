173534c3002e87ed3f8c30ce134e7bb0ce9cec05
Jan Brauner <you@example.com>
2025-07-27 20:25:52 +0200

    feat: added dialog and some information divs

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,37 @@
 
-function toggleSidebar() {
-    document.getElementById("pkm-sidebar").classList.toggle("active")
-}
+// function toggleSidebar() {
+//     document.getElementById("pkm-sidebar").classList.toggle("active")
+// }
 
+const dialog = document.querySelector("dialog")
 
 function openOverlay() {
-    let refOverlay = document.getElementById("overlay");
+    dialog.showModal()
+    // let openBtn = document.querySelector("#open-overlay");
+    // openBtn.addEventListener('click', () => dialog.showModal());
     // let hideScrollbar = document.getElementById("body-scrollbar");
     // hideScrollbar.classList.add("hide-scrollbar");
-    refOverlay.classList.remove("d-none");
-
 }
+
+function closeOverlay() {
+    dialog.close()
+    // let closeBtn = document.querySelector("#close-overlay");
+    // closeBtn.addEventListener('click', () => dialog.close());
+    // let hideScrollbar = document.getElementById("body-scrollbar");
+    // hideScrollbar.classList.add("hide-scrollbar");
+}
+
+//found in the internet
+dialog.addEventListener('click', event => {
+    const rect = dialog.getBoundingClientRect();
+    const isInDialog =
+        event.clientX >= rect.left &&
+        event.clientX <= rect.right &&
+        event.clientY >= rect.top &&
+        event.clientY <= rect.bottom;
+
+    if (!isInDialog) {
+        dialog.close();
+    }
+
+});
